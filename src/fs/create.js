57788360f3648eb1c240988dcf6e4ab3280eca76
Tanya <tatsiana.nevmerzhitskaya@ids-group.co.uk>
2022-11-30 01:30:16 +0300

    create_js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,12 @@
+import * as fsPromises from 'node:fs/promises';
+
 const create = async () => {
-    // Write your code here 
+
+    fsPromises.access('./src/fs/files/fresh.txt')
+        .then(() => {console.error(new Error('FS operation failed'))})
+        .catch(() => {
+            fsPromises.appendFile('./src/fs/files/fresh.txt', 'I am fresh and young');
+        });     
 };
 
 await create();
