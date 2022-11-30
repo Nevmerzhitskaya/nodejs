4621e64f9c160da15f06b2d38f41285a9a7efbe7
Tanya <tatsiana.nevmerzhitskaya@ids-group.co.uk>
2022-11-30 21:08:22 +0300

    delete_js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,16 @@
+import * as fsPromises from 'node:fs/promises';
+
 const remove = async () => {
-    // Write your code here 
+    const path = './src/fs/files/fileToRemove.txt';
+    const error = new Error('FS operation failed');
+    
+    try {
+
+        await fsPromises.unlink(path);
+
+    } catch (err) {
+        console.error(error.name + ':', error.message);
+    }
 };
 
 await remove();
