77fbe61bc49e97fe866aee90d9e0191c3e0483b8
Tanya <tatsiana.nevmerzhitskaya@ids-group.co.uk>
2022-11-30 21:29:24 +0300

    list_js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,32 @@
+import * as fsPromises from 'node:fs/promises';
+
 const list = async () => {
-    // Write your code here 
+
+    const pathSource = './src/fs/files/';
+    const error = new Error('FS operation failed');
+
+    try {
+        const isExistSource = await checkDir(pathSource);
+        if (isExistSource.error) throw error;
+
+        for await (const dirent of isExistSource.dir) {
+            console.log(dirent.name)
+        }
+
+    } catch (err) {
+        console.error(err.name + ':', err.message);
+    }
 };
 
 await list();
+
+async function checkDir(path) {
+    let dirInfo = {};
+    try {
+        dirInfo.dir = await fsPromises.opendir(path);
+        return dirInfo;
+    } catch (err) {
+        dirInfo.error = true;
+        return dirInfo;
+    }
+}
