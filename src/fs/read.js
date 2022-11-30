26acc9acd6eafb3d68d692ca9129d297f98a7fa3
Tanya <tatsiana.nevmerzhitskaya@ids-group.co.uk>
2022-11-30 21:35:52 +0300

    read_js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,15 @@
+import * as fsPromises from 'node:fs/promises';
+
 const read = async () => {
-    // Write your code here 
+    const path = './src/fs/files/fileToRead.txt';
+    const error = new Error('FS operation failed');
+    try {
+        const contents = await fsPromises.readFile(path, { encoding: 'utf8' });
+        console.log(contents);
+
+    } catch (err) {
+        console.error(error.name + ':', error.message);
+    }
 };
 
 await read();
