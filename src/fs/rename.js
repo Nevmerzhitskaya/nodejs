e00b23889f9db40c8f9ffffa0197b244f07a5c24
Tanya <tatsiana.nevmerzhitskaya@ids-group.co.uk>
2022-11-30 21:08:10 +0300

    rename_js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,34 @@
+import * as fsPromises from 'node:fs/promises';
+
 const rename = async () => {
-    // Write your code here 
+    const path = './src/fs/files/';
+    const oldFile = path + 'wrongFilename.txt';
+    const newFile = path + 'properFilename.md';
+    const error = new Error('FS operation failed');
+    
+    try {
+
+        const isExistSourceFile = await checkFile(oldFile);
+        if(!isExistSourceFile) throw error;
+
+        const isExistResultFile = await checkFile(newFile);
+        if(isExistResultFile) throw error;
+
+        await fsPromises.rename(oldFile, newFile);
+
+    } catch (err) {
+        console.error(err.name + ':', err.message);
+    }
 };
 
 await rename();
+
+async function checkFile(path) {
+    try {
+        await fsPromises.access(path);
+
+        return true;
+    } catch (err) {
+        return false;
+    }
+}
