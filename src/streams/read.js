25130c9c0b82258991464d2574200956c981fba7
Tanya <tatsiana.nevmerzhitskaya@ids-group.co.uk>
2022-12-02 17:31:57 +0300

    read_js_stream

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,11 @@
+import { createReadStream } from 'node:fs';
+
 const read = async () => {
-    // Write your code here 
+    const readStream = createReadStream('./src/streams/files/fileToRead.txt').pipe(process.stdout);
+
+    readStream.on('error', function (err) {
+        console.log(err);
+    });
 };
 
 await read();
