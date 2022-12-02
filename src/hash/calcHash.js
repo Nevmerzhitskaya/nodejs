0378d46e522b00148007cd1522e219882be8aa7a
Tanya <tatsiana.nevmerzhitskaya@ids-group.co.uk>
2022-12-02 17:31:31 +0300

    calcHash_js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,13 +1,14 @@
-import { createReadStream } from 'node:fs';
+import { readFile } from 'node:fs/promises';
 
 const { createHash } = await import('node:crypto');
 
 const calculateHash = async () => {
     const hash = createHash('sha256');
-    const input = createReadStream('./src/hash/files/fileToCalculateHashFor.txt');    
-    const hex = input.pipe(hash).digest('hex');
-
-    console.log(hex);
+    const filePath = './src/hash/files/fileToCalculateHashFor.txt';
+    const contents = await readFile(filePath, { encoding: 'utf8' });
+    const hex = hash.update(contents);
+    
+    console.log(hex.digest('hex'));
 };
 
 await calculateHash();
