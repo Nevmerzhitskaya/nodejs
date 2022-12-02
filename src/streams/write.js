1f3ac12799cf7b1500a4715f7b75d326d1619eb4
Tanya <tatsiana.nevmerzhitskaya@ids-group.co.uk>
2022-12-02 17:32:23 +0300

    write_js_stream

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,12 @@
+import { createWriteStream } from 'node:fs';
+
 const write = async () => {
-    // Write your code here 
+    const writeStream = createWriteStream('./src/streams/files/fileToWrite.txt');
+    process.stdin.pipe(writeStream);
+
+    writeStream.on('error', function (err) {
+        console.log(err);
+    });
 };
 
 await write();
