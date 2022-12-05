f98dd9570abef362f0c6ebad95c7f08d0e0973dd
Tanya <lamara@tut.by>
2022-12-05 19:55:50 +0300

    calcHash_js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,10 +1,11 @@
 import { readFile } from 'node:fs/promises';
+import { filePathConverter } from '../filePathConverter.js';
 
 const { createHash } = await import('node:crypto');
+const filePath = filePathConverter('hash','files','fileToCalculateHashFor.txt');
 
 const calculateHash = async () => {
     const hash = createHash('sha256');
-    const filePath = './src/hash/files/fileToCalculateHashFor.txt';
     const contents = await readFile(filePath, { encoding: 'utf8' });
     const hex = hash.update(contents);
     
