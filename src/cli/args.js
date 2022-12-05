3b58e9868e273909489b72bda93c74098a861627
Tanya <tatsiana.nevmerzhitskaya@ids-group.co.uk>
2022-12-05 17:29:34 +0300

    args_js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,9 +1,9 @@
 const parseArgs = () => {
     const argv = process.argv;
 
-    for(let i=0; i<argv.length; i++) {
-        if(argv[i].startsWith('--') && argv[i+1]) {
-            console.log(`${argv[i].substring(2)} is ${argv[i+1]}`);
+    for (let i = 0; i < argv.length; i++) {
+        if (argv[i].startsWith('--') && argv[i + 1]) {
+            console.log(`${argv[i].substring(2)} is ${argv[i + 1]}`);
             i++;
         }
     }
