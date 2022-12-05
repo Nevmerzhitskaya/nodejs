35283678bbaa0ac24f2a5b93fd292c4145548f7c
Tanya <tatsiana.nevmerzhitskaya@ids-group.co.uk>
2022-12-05 17:29:56 +0300

    env_js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,13 +1,12 @@
 const parseEnv = () => {
-    // console.log(process.env.RSS_*);
     let envString = '';
 
-    for (const key in process.env ) {
-        if(key.startsWith('RSS_')) {
+    for (const key in process.env) {
+        if (key.startsWith('RSS_')) {
             envString += (envString != '') ? `; ${key}=${process.env[key]}` : `${key}=${process.env[key]}`;
-
         }
     }
+
     console.log(envString);
 };
 
