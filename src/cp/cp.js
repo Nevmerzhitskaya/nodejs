8814c681c452d6a44bb037d97c8809d8a94e7154
Tanya <tatsiana.nevmerzhitskaya@ids-group.co.uk>
2022-12-05 17:30:13 +0300

    cp_js

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,15 @@
+import { spawn } from 'node:child_process';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);    
+const filePath = path.join(__dirname, 'files', 'script.js');
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    spawn('node', [filePath, ...args], {
+        stdio: [process.stdin, process.stdout, process.stderr]
+    });
 };
 
-spawnChildProcess();
+spawnChildProcess(['arg1', 'arg2', 'arg3']);
