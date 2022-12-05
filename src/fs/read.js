abfc5c4792ac8a8f5ad994ccce395ce5883825c1
Tanya <lamara@tut.by>
2022-12-05 19:55:18 +0300

    read_js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,8 +1,10 @@
 import * as fsPromises from 'node:fs/promises';
+import { filePathConverter } from '../filePathConverter.js';
 
 const read = async () => {
-    const path = './src/fs/files/fileToRead.txt';
     const error = new Error('FS operation failed');
+    const path = filePathConverter('fs','files','fileToRead.txt');
+
     try {
         const contents = await fsPromises.readFile(path, { encoding: 'utf8' });
         console.log(contents);
