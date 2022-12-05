23d62af7500d0841e2b484d4af8b49a2303da7b7
Tanya <lamara@tut.by>
2022-12-05 19:54:02 +0300

    cp_js

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,10 +1,7 @@
 import { spawn } from 'node:child_process';
-import path, { dirname } from 'path';
-import { fileURLToPath } from 'url';
+import { filePathConverter } from '../filePathConverter.js';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);    
-const filePath = path.join(__dirname, 'files', 'script.js');
+const filePath = filePathConverter('cp','files','script.js');
 
 const spawnChildProcess = async (args) => {
     spawn('node', [filePath, ...args], {
