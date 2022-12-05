7436419bb823ed18aa0c26f45f3739707eed827d
Tanya <tatsiana.nevmerzhitskaya@ids-group.co.uk>
2022-12-05 17:32:18 +0300

    decompress_js

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,13 @@
+import { createUnzip } from 'node:zlib';
+import { createReadStream, createWriteStream } from 'node:fs';
+
 const decompress = async () => {
-    // Write your code here 
+    const filePath = './src/zip/files/';
+    const unzip = createUnzip();
+    const source = createReadStream(filePath + 'archive.gz');
+    const destination = createWriteStream(filePath + 'fileToCompress.txt');
+
+    source.pipe(unzip).pipe(destination);
 };
 
 await decompress();
