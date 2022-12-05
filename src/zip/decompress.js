a74861e4580cd0ecc926adbcc0edcf80c9d8a708
Tanya <lamara@tut.by>
2022-12-05 19:57:02 +0300

    decompress_js

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,11 +1,14 @@
 import { createUnzip } from 'node:zlib';
 import { createReadStream, createWriteStream } from 'node:fs';
+import { filePathConverter } from '../filePathConverter.js';
+
+const sourcePath = filePathConverter('zip','files','archive.gz');
+const destinationPath = filePathConverter('zip','files','fileToCompress.txt');
 
 const decompress = async () => {
-    const filePath = './src/zip/files/';
     const unzip = createUnzip();
-    const source = createReadStream(filePath + 'archive.gz');
-    const destination = createWriteStream(filePath + 'fileToCompress.txt');
+    const source = createReadStream(sourcePath);
+    const destination = createWriteStream(destinationPath);
 
     source.pipe(unzip).pipe(destination);
 };
