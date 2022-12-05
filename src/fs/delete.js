dc0f85a95709d4d7f71c1cf9b10176089f9a6c5e
Tanya <lamara@tut.by>
2022-12-05 19:54:59 +0300

    delete_js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,12 +1,13 @@
 import * as fsPromises from 'node:fs/promises';
+import { filePathConverter } from '../filePathConverter.js';
 
 const remove = async () => {
-    const path = './src/fs/files/fileToRemove.txt';
     const error = new Error('FS operation failed');
+    const filePath = filePathConverter('fs','files','fileToRemove.txt');
     
     try {
 
-        await fsPromises.unlink(path);
+        await fsPromises.unlink(filePath);
 
     } catch (err) {
         console.error(error.name + ':', error.message);
