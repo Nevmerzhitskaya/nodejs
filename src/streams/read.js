95277861949a3270c04a67f2a3bb35b589ff658a
Tanya <lamara@tut.by>
2022-12-05 19:56:23 +0300

    read_js_streams

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,7 +1,10 @@
 import { createReadStream } from 'node:fs';
+import { filePathConverter } from '../filePathConverter.js';
+
+const filePath = filePathConverter('streams','files','fileToRead.txt');
 
 const read = async () => {
-    const readStream = createReadStream('./src/streams/files/fileToRead.txt').pipe(process.stdout);
+    const readStream = createReadStream(filePath).pipe(process.stdout);
 
     readStream.on('error', function (err) {
         console.log(err);
