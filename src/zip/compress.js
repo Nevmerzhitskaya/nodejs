a7af8286689314f5a6ad1e596530aa3f385ec4ee
Tanya <tatsiana.nevmerzhitskaya@ids-group.co.uk>
2022-12-05 17:32:10 +0300

    compress_js

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,13 @@
+import { createGzip } from 'node:zlib';
+import { createReadStream, createWriteStream } from 'node:fs';
+
 const compress = async () => {
-    // Write your code here 
+    const filePath = './src/zip/files/';
+    const gzip = createGzip();
+    const source = createReadStream(filePath + 'fileToCompress.txt');
+    const destination = createWriteStream(filePath + 'archive.gz');
+    
+    source.pipe(gzip).pipe(destination);
 };
 
 await compress();
